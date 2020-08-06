d8017d9ba93d5314abfaa9869f7d441c8f89e693
Krishna Satya <vadapallikrishna@gmail.com>
2020-08-06 13:36:37 +0530

    Hello

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-function createElement(component, props, children) {
-}
-var Router = /** @class */ (function () {
-    function Router() {
+"use strict";
+exports.__esModule = true;
+exports.createElement = exports.Kwab = void 0;
+var Kwab = /** @class */ (function () {
+    function Kwab(el) {
+        this.el = el;
+        this.routes = [];
     }
-    return Router;
+    Kwab.getInstance = function () {
+        if (!Kwab.instance)
+            throw Error("no instance found");
+        return Kwab.instance;
+    };
+    Kwab.prototype.appendRoute = function (path, component) {
+        this.routes.push({ "path": path, "component": component });
+    };
+    Kwab.prototype.getView = function (path) {
+        var component = this.routes.find(function (i) { return i["path"] == path; }).component;
+        this.el.appendChild(document.createElement(component));
+    };
+    return Kwab;
 }());
+exports.Kwab = Kwab;
+window.addEventListener("popstate", function (event) {
+    var instance = Kwab.getInstance();
+    instance.getView(window.location.pathname);
+});
+function createElement(component, props, children) {
+    var el = document.createElement(component);
+    el = Object.assign(el, props);
+    el.append.apply(el, children);
+    return el;
+}
+exports.createElement = createElement;
+function route(pathname, component) {
+    var instance = Kwab.getInstance();
+    instance.appendRoute(pathname, component);
+}
--- index.ts
+++ index.ts
@@ -1,10 +1,45 @@
 
-function createElement(component,props,children) {
+class Kwab{
+    private static instance: Kwab;
+    private el: HTMLElement;
+    private routes: any;
+    constructor(el: any) {
+        this.el = el;
+	this.routes = [];
+    }
+
+    static getInstance(): Kwab {
+	if(!Kwab.instance)
+		throw Error("no instance found");
+	return Kwab.instance;
+    }
+
+    appendRoute(path: string, component: string) {
+      	this.routes.push({"path": path,"component": component});
+    }
+    
+    getView(path: string) {
+       let { component } = this.routes.find(i => i["path"] == path);
+       this.el.appendChild(document.createElement(component));    
+    }
 
 }
 
-class Router {
-     constructor() {
+window.addEventListener("popstate", function(event: any) {
+      	let instance = Kwab.getInstance();
+    	instance.getView(window.location.pathname);
+});
 
-     }
+function createElement(component: any,props: any,children: any) {
+         let el = document.createElement(component);
+	 el = (<any>Object).assign(el,props);
+	 el.append(...children);
+	 return el;
 }
+
+function route(pathname: string, component: string) {
+	let instance = Kwab.getInstance();
+	instance.appendRoute(pathname, component);
+}
+
+export {Kwab, createElement};
